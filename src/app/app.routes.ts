cb9efd35c8ab262196ba9e871f7a8041672162cf
camden.warburton <camden.warburton@ucalgary.ca>
2026-06-28 22:57:02 -0600

    Tailwind installed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,25 @@
 import { Routes } from '@angular/router';
 
+import { Layout } from './components/layout/layout';
+
+// Pages
+import { DatesPage } from './pages/dates-page/dates-page';
+import { GoalsPage } from './pages/goals-page/goals-page';
 import { HomePage } from './pages/home-page/home-page';
+import { RecipesPage } from './pages/recipes-page/recipes-page';
+
+import { NotFound } from './pages/not-found/not-found';
 
 export const routes: Routes = [
-    { path: '', component: HomePage }
+  {
+    path: '',
+    component: Layout,
+    children: [
+      { path: '', component: HomePage },
+      { path: 'dates', component: DatesPage },
+      { path: 'goals', component: GoalsPage },
+      { path: 'recipes', component: RecipesPage },
+    ],
+  },
+  { path: '**', component: NotFound },
 ];
--- src/app/components/layout/layout.ts
+++ src/app/components/layout/layout.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
-export class Layout {}
+export class Layout {
+  // list of pages: [route, title]
+  private pageList: Array<{ route: string; title: string }> = [
+    { route: '', title: 'Home' },
+    { route: 'dates', title: 'Dates' },
+    { route: 'goals', title: 'Goals' },
+    { route: 'recipes', title: 'Recipes' },
+  ];
+
+}
